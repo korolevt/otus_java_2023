0bf90ffbd5db9510506ff23cf053d4360e2bee8d
tkorolev <tkorolev@callinsight.ru>
2023-07-09 12:32:35 +0300

    HW-15  SensorDataProcessorBuffered
        Очередь реализована с помощью PriorityBlockingQueue

--- hw15-queue/src/main/java/org/kt/services/processors/SensorDataProcessorBuffered.java
+++ hw15-queue/src/main/java/org/kt/services/processors/SensorDataProcessorBuffered.java
@@ -6,30 +6,38 @@ import org.kt.api.SensorDataProcessor;
 import org.kt.api.model.SensorData;
 import org.kt.lib.SensorDataBufferedWriter;
 
-// Этот класс нужно реализовать
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.concurrent.*;
+
 public class SensorDataProcessorBuffered implements SensorDataProcessor {
     private static final Logger log = LoggerFactory.getLogger(SensorDataProcessorBuffered.class);
 
     private final int bufferSize;
     private final SensorDataBufferedWriter writer;
+    private final PriorityBlockingQueue<SensorData> dataBuffer;
 
     public SensorDataProcessorBuffered(int bufferSize, SensorDataBufferedWriter writer) {
         this.bufferSize = bufferSize;
         this.writer = writer;
+        dataBuffer = new PriorityBlockingQueue<>(bufferSize, Comparator.comparing(SensorData::getMeasurementTime));
     }
 
     @Override
     public void process(SensorData data) {
-    /*
         if (dataBuffer.size() >= bufferSize) {
             flush();
         }
-    */
+        dataBuffer.add(data);
     }
 
     public void flush() {
         try {
-            //writer.writeBufferedData(bufferedData);
+            ArrayList<SensorData> data = new ArrayList<>();
+            dataBuffer.drainTo(data);
+            if (!data.isEmpty()) {
+                writer.writeBufferedData(data);
+            }
         } catch (Exception e) {
             log.error("Ошибка в процессе записи буфера", e);
         }
