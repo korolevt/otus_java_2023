2f45ea4b7259ed32c8c4aa2583b061c1d8a6fc90
tkorolev <tkorolev@callinsight.ru>
2023-07-25 14:09:55 +0300

    Правки в logback

--- hw17-executors/build.gradle.kts
+++ hw17-executors/build.gradle.kts
@@ -4,10 +4,5 @@ plugins {
 
 dependencies {
     implementation("ch.qos.logback:logback-classic")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
 
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
-}
